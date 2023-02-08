9fa048d242df7a0994459caa6611e2b369ed7edd
KolejnyDeveloper <78930863+KolejnyDeveloper@users.noreply.github.com>
2023-02-08 11:01:53 +0100

    Code
    code

---
+++ Nakladka2Test/Ikonawzasobniku.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nakladka2Test
+{
+    class Ikonawzasobniku
+    {
+        private NotifyIcon notifyIcon;
+        private System.Windows.Window okno;
+
+        public Ikonawzasobniku(System.Windows.Window okno)
+        {
+            this.okno = okno;
+            string nazwaIko = "b.ico";
+            string nazwaApp = Application.ProductName;
+            System.Windows.Resources.StreamResourceInfo sri = System.Windows.Application.GetResourceStream(new Uri(@"/" + nazwaApp + ";component/" + nazwaIko, UriKind.RelativeOrAbsolute));
+            Icon ikona = new Icon(sri.Stream);
+
+            ContextMenuStrip menu = makemenu();
+
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = ikona;
+            notifyIcon.Text = "Cos";
+            notifyIcon.ContextMenuStrip = menu;
+            notifyIcon.Visible = true;
+
+            notifyIcon.DoubleClick += (s, e) =>
+            {
+                notifyIcon.BalloonTipTitle = "Test";
+                notifyIcon.BalloonTipIcon = ToolTipIcon.Info;
+                notifyIcon.BalloonTipText = "Cześć";
+                notifyIcon.ShowBalloonTip(2000);
+            };
+
+            okno.MouseRightButtonDown += (s, e) =>
+            {
+                System.Windows.Point p = okno.PointToScreen(e.GetPosition(okno));
+                menu.Show((int)p.X, (int)p.Y);
+            };
+        }
+
+        private ContextMenuStrip makemenu()
+        {
+            ContextMenuStrip menu = new ContextMenuStrip();
+
+            ToolStripMenuItem ukryjtsmi = new ToolStripMenuItem("Schowaj");
+            ukryjtsmi.Click += (s, e) => { okno.Hide(); };
+            menu.Items.Add(ukryjtsmi);
+            ToolStripMenuItem wroctsmi = new ToolStripMenuItem("Wroc"); //do mnie (niech skonczy sie ten dziwny sen)");
+            wroctsmi.Click += (s, e) => { okno.Show(); };
+            menu.Items.Add(wroctsmi);
+            ToolStripMenuItem zamktsmi = new ToolStripMenuItem("Zamknij");
+            zamktsmi.Click += (s, e) => { okno.Close(); };
+            menu.Items.Add(zamktsmi);
+
+            return menu;
+        }
+
+        private bool Widoczna
+        {
+            get
+            {
+                return notifyIcon.Visible;
+            }
+            set
+            {
+                notifyIcon.Visible = value;
+            }
+        }
+
+        public void Usun()
+        {
+            notifyIcon.Visible = false;
+            notifyIcon.Dispose();
+            notifyIcon = null; 
+        }
+    }
+}
---
+++ Nakladka2Test/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Nakladka2Test
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Ikonawzasobniku ikonawzasobniku;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ikonawzasobniku = new Ikonawzasobniku(this);
+        }
+        #region Przenoszenie okna
+        private bool przenos = false;
+        private Cursor cursor = null;
+        private Point poczatkowy;
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ButtonState == Mouse.LeftButton)
+            {
+                przenos = true;
+                cursor = this.Cursor;
+                Cursor = Cursors.Hand;
+                poczatkowy = e.GetPosition(this);
+            }
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (przenos)
+            {
+                Vector ruch = e.GetPosition(this) - poczatkowy;
+                Left += ruch.X;
+                Top += ruch.Y;
+            }
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            //this.Width = System.Windows.SystemParameters.PrimaryScreenWidth;
+            //this.Height = System.Windows.SystemParameters.PrimaryScreenHeight;
+            this.Topmost = true;
+            //this.Top = 0;
+            //this.Left = 0;
+        }
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            Window window = (Window)sender;
+            window.Topmost = true;
+            this.Activate();
+        }
+        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if(przenos)
+            {
+                Cursor = cursor;
+                przenos = false;
+            }
+        }
+        #endregion
+        bool zakanim = false;
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Escape)
+            {
+               this.Close();
+            }
+        }
+
+        
+        
+        private void okno_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (!zakanim)
+            {
+                Storyboard Znikanie = this.Resources["Znikanie"] as Storyboard;
+                Znikanie.Begin();
+                e.Cancel = true;
+            }
+        }
+        private void Storyboard_Completed(object sender, EventArgs e)
+        {
+            zakanim = true;
+            ikonawzasobniku.Usun();
+            //e.Cancel = false;
+            Close();
+        }
+
+        private void okno_PreviewLostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            var window = (Window)sender;
+            window.Topmost = true;
+        }
+
+        
+       
+    }
+}
